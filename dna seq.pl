1f29bde8facc85f126c714630da1a45326606cfb
NikithaSunkara <114140256+NikithaSunkara@users.noreply.github.com>
2022-09-22 19:02:46 +0530

    Add files via upload

---
+++ dna seq.pl
@@ -0,0 +1,9 @@
+$DNA_seq="ATGCTGTA";
+$DNA_seq=~tr/ATGC/atgc/;
+print "$DNA_seq\n";
+$dna_seq="atgccgta";
+$dna_seq=~tr/atgc/ATGC/;
+print "$dna_seq\n";
+$DNA_seq="ATGCTGTA";
+$DNA_seq=~tr/ATGC/atgc/;
+print "$DNA_seq\n";
